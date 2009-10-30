380f636f56ecc6296e423dca518a2628090c35c5
Jorge Calás Lozano <calas@railsdog.com>
2009-10-30 13:00:52 +0100

    Fixed errors that prevent integration test to run correctly.
    ***ALL INTEGRATION TESTS ARE STILL FAILING***

--- test/integration/searching_test.rb
+++ test/integration/searching_test.rb
@@ -1,17 +1,19 @@
+require 'test_helper'
+
 class SearchingTest < ActionController::IntegrationTest
   context "Search" do
     setup do
+      Factory(:admin_user)
       @numbers = %w{one two three four five six}
       @taxonomy = Taxonomy.find_or_create_by_name("test_taxonomy")
       @taxons = (0..1).map{|x|
         Taxon.find_by_name("test_taxon_#{x}") ||
-          Taxon.create(:name => "test_taxon_#{x}", :taxonomy_id => @taxonomy.id)
+        Taxon.create(:name => "test_taxon_#{x}", :taxonomy_id => @taxonomy.id)
       }
       @products = (0..4).map do |x|
         pr = Factory(:product,
-          :price => (x+1)*10,
-          :name => "test product #{@numbers[x]}"
-        )
+                     :price => (x+1)*10,
+                     :name => "test product #{@numbers[x]}")
         pr.taxons << @taxons[x%2]
         pr
       end
@@ -28,26 +30,26 @@ class SearchingTest < ActionController::IntegrationTest
       should "find products based on more complex criteria" do
         get '/s/master_price_lt/30/name_contains/one'
         cheap_products_with_one = Product.find(:all,
-          :include => :master,
-          :conditions => "variants.price < 30 AND product.name LIKE '%one%'"
-        )
+                                               :include => :master,
+                                               :conditions => "variants.price < 30 AND products.name LIKE '%one%'")
 
         assert_equal(cheap_products_with_one, assigns(:products))
       end
 
       should "be able to be ordered" do
         get '/s/master_price_lt/30/name_contains/one/descend_by_id'
+
         cheap_products_with_one = Product.find(:all,
-          :include => :master,
-          :conditions => "variants.price < 30 AND product.name LIKE '%one%'",
-          :order => "products.id DESC"
-        )
+                                               :include => :master,
+                                               :conditions => "variants.price < 30 AND products.name LIKE '%one%'",
+                                               :order => "products.id DESC")
 
         assert_equal(cheap_products_with_one, assigns(:products))
       end
 
       should "find products using any" do
         get '/s/name_like_any/one,two,five'
+
         products = %w{one two five}.map{|name|
           Product.find(:all, :conditions => ["name LIKE ?", "%#{name}%"])
         }.flatten
@@ -105,6 +107,7 @@ class SearchingTest < ActionController::IntegrationTest
 
     context "product group search" do
       setup do
+        Factory(:admin_user)
         @pg = ProductGroup.from_url('/s/name_like_any/one,two,five/master_price_lt/30/descend_by_name')
         @pg.name = "TestProductGroup"
         @pg.save
@@ -121,6 +124,7 @@ class SearchingTest < ActionController::IntegrationTest
 
       should "find products using named product group in taxon" do
         get '/t/test_taxon_0/pg/TestProductGroup'
+
         products = %w{one five}.map{|name|
           Product.find(:all, :conditions => ["name LIKE ?", "%#{name}%"])
         }.flatten.reject{|pr| pr.master.price >= 30}.sort_by{|pr| pr.name}
